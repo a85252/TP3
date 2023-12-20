1cfa3758888d5560f53885bda0055c182cf7dc1e
a85252 <a85252@ualg.pt>
2023-12-20 18:52:01 +0000

    Alterado a navbar e adicionado novas paginas

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -64,24 +64,21 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'Menu',
-        logo: {
-          alt: 'My Site Logo',
-          src: 'img/car-logo.png',
+      title: 'Menu',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/car-logo.png',
+      },
+      items: [
+        {to: '/docs/servicos-oficina', label: 'Serviços de Oficina', position: 'left'},
+        {to: '/docs/equipamento-manutencao', label: 'Equipamento e Manutenção', position: 'left'},
+        {to: '/docs/o-seu-veiculo', label: 'O Seu Veículo', position: 'left'},
+        {to: '/docs/suporte', label: 'Suporte', position: 'left'},
+        {
+          href: 'https://github.com/facebook/docusaurus',
+          label: 'GitHub',
+          position: 'right',
         },
-        items: [
-          {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Tutorial',
-          },
-          {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
-          },
         ],
       },
       footer: {
