ed1830d903551e4340c236f4f1c46a17acc72ea8
a85252 <a85252@ualg.pt>
2023-12-20 15:21:40 +0000

    Alterado o logotipo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -67,7 +67,7 @@ const config = {
         title: 'Menu',
         logo: {
           alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          src: 'img/car-logo.png',
         },
         items: [
           {
